5336799157eafaa6f15a83cfd6e9cdcb8018b5e3
Kenny Ikeji <kikeji@college.harvard.edu>
2025-08-31 07:56:55 -0700

    just warming up

---
+++ app/test/page.tsx
@@ -0,0 +1,10 @@
+import SwitchPageButton from "@/components/SwitchPageButton"
+
+export default function LetsSee() {
+    return (
+        <div>
+            <h1>I wonder if this is going to work</h1>
+            <SwitchPageButton link="/" />
+        </div>
+    )
+};
